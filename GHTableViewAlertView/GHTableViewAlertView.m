b3795756ee107c8eeb9c1b03255d7c002e4021a5
Oliver Letterer <oliver.letterer@googlemail.com>
2011-08-03 10:21:25 +0200

    setting self as delegate and datasource for _tableView

--- GHTableViewAlertView/GHTableViewAlertView.m
+++ GHTableViewAlertView/GHTableViewAlertView.m
@@ -21,12 +21,13 @@
 - (id)initWithTitle:(NSString *)title message:(NSString *)message delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... {
     if (self = [super initWithTitle:title message:@"\n\n\n\n\n\n\n" delegate:delegate cancelButtonTitle:cancelButtonTitle otherButtonTitles:otherButtonTitles, nil]) {
         _tableView = [[UITableView alloc] initWithFrame:CGRectMake(11.0f, 50.0f, 261.0f, 147.0f) style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
 		_tableView.layer.cornerRadius = 5.0f;
 		_tableView.separatorColor = [UIColor colorWithWhite:175.0f/255.0f alpha:0.0f];
 		_tableView.backgroundColor = [UIColor whiteColor];
 		[self addSubview:_tableView];
         _tableView.backgroundView = [[GHTableViewAlertViewTableViewBackgroundView alloc] initWithFrame:_tableView.bounds];
-		_tableView.delegate = self;
         
         UIImage *image = [[UIImage imageNamed:@"GHAlertViewOverlayImage.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(7.0f, 5.0f, 6.0f, 5.0f)];
         _overlayImageView = [[UIImageView alloc] initWithImage:image];
